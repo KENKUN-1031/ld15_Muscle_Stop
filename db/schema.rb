8064af28273800b9f5c53444c56a5699ebba86b9
EC2 Default User <ec2-user@ip-10-1-8-177.ap-northeast-1.compute.internal>
2023-06-15 18:16:55 +0000

    final

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2023_05_19_215052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "activity_logs", force: :cascade do |t|
     t.string "user_id"
